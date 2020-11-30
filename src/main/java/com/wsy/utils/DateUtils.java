23027716dca6d50834a633a480d43f2fed43d330
wsy <wush126@163.com>
2020-11-30 20:13:43 +0800

    Initial commit

--- src/main/java/com/wsy/utils/DateUtils.java
+++ src/main/java/com/wsy/utils/DateUtils.java
@@ -1,4 +1,13 @@
 package com.wsy.utils;
 
 public class DateUtils {
+
+    //常用的日期格式
+    //年月日
+    private static final String DATE = "yyyy-MM-dd";
+    private static final String DATE_TIME = "yyyy-MM-dd hh:mm:ss";
+
+
+
+
 }
